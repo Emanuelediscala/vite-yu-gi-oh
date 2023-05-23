84a51bd2caccb9dd29738f6839df993317aec934
Emanuele discala <manueldiscala@gmail.com>
2023-05-23 19:30:07 +0200

    All Done

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import AppHeader from "./components/AppHeader.vue"
 import AppContent from "./components/AppContent.vue"
 import AppFooter from "./components/Appfooter.vue"
-
+import {store} from "./components/data/store.js"
 import axios from 'axios';
 
 export default {
@@ -10,6 +10,24 @@ export default {
     AppHeader,
     AppContent,
     AppFooter
+  },
+  data() {
+    return {
+   store
+    }
+  },
+  methods: {
+    CardsLoaded() {
+                axios.get(this.store.urlApi).then((risposta => {
+                    this.store.cards = risposta.data.data;
+                    this.store.cards.num = this.store.cards[0].length 
+                    console.log(this.store.cards);
+                }))
+            
+    }
+  },
+  mounted() {
+    this.CardsLoaded()
   }
 }
 
--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,15 +1,34 @@
+<script>
+import {store} from "./data/store"
+
+export default {
+    data() {
+        return {
+            store
+        }
+        
+    }
+}
+
+</script>
 <template>
     <main>
-        <div class="container">
-            <option value="">
-                <select name="" id="">Alien</select>
-                <select name="" id=""></select>
-                <select name="" id=""></select>
-            </option>
-            <div id="cardContainer" class="container p-3 bg-white ">
-                <div class="bg-black text-white p-2">
-                    <p>Found  39 Cards</p>
+        <div class="container p-2">
+            <select name="" id="" class="mb-3">
+                <option value="">Alien</option>
+            </select>
+            <div id="cardContainer" class="row container p-2 bg-white ">
+                <div class="bg-black text-white p-2 mb-3">
+                    <p v-show="this.store.cards.length" class="m-0">Found {{ this.store.cards.length }} cards</p>
                 </div>
+                    <div v-if="this.store.cards < 50"><img src="../assets/Spinner-1s-200px.gif" alt=""></div>
+                    <div id="Cards" v-if="this.store.cards.length === 50" class="mb-2" v-for="(card,i) in this.store.cards">
+                        <img :src="card.card_images[0].image_url" alt="">
+                        <div class="bg-Coral text-center p-2">
+                        <p class="text-white">{{ card.name }}</p>
+                        <span>{{ card.archetype }}</span>
+                        </div>
+                    </div>
             </div>
         </div>
     </main>
@@ -19,4 +38,16 @@
 main {
     background-color: coral;
 }
+.bg-Coral {
+    background-color: coral;
+}
+
+img {
+    width: 100%;
+    height: 280px;
+}
+#Cards {
+    width: calc(100% / 5);
+}
+
 </style>
